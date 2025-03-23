e71c856980143946b3c8401c849d88373acb0392
DCjanus <DCjanus@dcjanus.com>
2025-03-23 12:20:32 +0800

    feat: add ListProjects method to Dida client for project retrieval; update go.mod with new dependencies

--- internal/dida/client.go
+++ internal/dida/client.go
@@ -32,6 +32,7 @@ func (c *Client) GetTask(ctx context.Context, projectID, taskID string) (*api.Ta
 
 	res, err := c.cli.
 		R().
+		SetContext(ctx).
 		SetPathParams(map[string]string{
 			"project_id": projectID,
 			"task_id":    taskID,
@@ -46,4 +47,23 @@ func (c *Client) GetTask(ctx context.Context, projectID, taskID string) (*api.Ta
 	}
 
 	return &reply, nil
+}
+
+func (c *Client) ListProjects(ctx context.Context) ([]*api.Project, error) {
+	c.log.Debug("ListProjects")
+	path := "/open/v1/project"
+
+	reply := []*api.Project{}
+	res, err := c.cli.
+		R().
+		SetContext(ctx).
+		SetResult(&reply).
+		Get(path)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to list projects")
+	}
+	if res.IsError() {
+		return nil, errors.Errorf("failed to list projects, status: %d, body: %s", res.StatusCode(), res.String())
+	}
+	return reply, nil
 }
